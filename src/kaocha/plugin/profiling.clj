690b170c7df42e3e45d806d664459f53ead1e4d1
Arne Brasseur <arne@arnebrasseur.net>
2018-07-13 18:53:54 +0200

    Make the profiling plugin a lot nicer
    Print total time spent for each type, and make the amount of slow tests to show
    configurable.

--- src/kaocha/plugin/profiling.clj
+++ src/kaocha/plugin/profiling.clj
@@ -6,47 +6,67 @@
             [clojure.java.io :as io])
   (:import java.time.Instant))
 
+(defn start [testable]
+  (assoc testable ::start (Instant/now)))
+
+(defn stop [testable]
+  (cond-> testable
+    (::start testable)
+    (assoc ::duration (jt/time-between (::start testable)
+                                       (Instant/now)
+                                       :nanos))))
 
 (defplugin kaocha.plugin/profiling
-  (pre-test [testable test-plan]
-    (assoc testable ::start (Instant/now)))
+  (pre-run [test-plan]
+    (start test-plan))
 
-  (post-test [testable test-plan]
-    (cond-> testable
-      (::start testable)
-      (assoc ::duration (jt/time-between (::start testable)
-                                         (Instant/now)
-                                         :nanos))))
+  (post-run [test-plan]
+    (stop test-plan))
 
-  (post-run [result]
+  (pre-test [testable _]
+    (start testable))
+
+  (post-test [testable _]
+    (stop testable))
+
+  (config [config]
+    (assoc config ::limit (::limit config 3)))
+
+  (post-summary [result]
     (let [tests (remove :kaocha.test-plan/load-error (testable/test-seq result))
-          types (group-by :kaocha.testable/type tests)]
-      (doseq [[type tests] types
-              :when type
-              :let [slowest (take 3 (reverse (sort-by ::duration tests)))]]
-        (when type
-          (println "Slowest" type)
-          (doseq [test slowest
-                  :let [duration (::duration test)
-                        cnt (count (:kaocha.result/tests test))]]
-            (when duration
-              (if (> cnt 0)
-                (println
-                 (format "  %s\n    \033[1m%.5f seconds\033[0m average (%.5f seconds / %d tests)"
-                         (subs (str (:kaocha.testable/id test)) 1)
-                         (float (/ duration cnt 1e9))
-                         (float (/ duration 1e9))
-                         cnt))
+          types (group-by :kaocha.testable/type tests)
+          total-dur (::duration result)
+          limit (::limit result)]
+      (->> (for [[type tests] types
+                 :when type
+                 :let [slowest (take limit (reverse (sort-by ::duration tests)))
+                       slow-test-dur (apply + (keep ::duration slowest))]]
+             [(format "\nTop %s slowest %s (%.5f seconds, %.1f%% of total time)\n"
+                      (count slowest)
+                      (subs (str type) 1)
+                      (float (/ slow-test-dur 1e9))
+                      (float (* (/ slow-test-dur total-dur) 100)))
+              (for [test slowest
+                    :let [duration (::duration test)
+                          cnt (count (:kaocha.result/tests test))]
+                    :when duration]
+                (if (> cnt 0)
+                  (format "  %s\n    \033[1m%.5f seconds\033[0m average (%.5f seconds / %d tests)\n"
+                          (subs (str (:kaocha.testable/id test)) 1)
+                          (float (/ duration cnt 1e9))
+                          (float (/ duration 1e9))
+                          cnt)
 
-                (when (:file (:kaocha.testable/meta test))
-                  (println
-                   (format "  %s\n    \033[1m%.5f seconds\033[0m %s:%d"
-                           (subs (str (:kaocha.testable/id test)) 1)
-                           (float (/ duration 1e9))
-                           (str/replace (:file (:kaocha.testable/meta test))
-                                        (str (.getCanonicalPath (io/file ".")) "/")
-                                        "")
-                           (:line (:kaocha.testable/meta test))))) ))
-            )
-          (println))))
+                  (when (:file (:kaocha.testable/meta test))
+                    (format "  %s\n    \033[1m%.5f seconds\033[0m %s:%d\n"
+                            (subs (str (:kaocha.testable/id test)) 1)
+                            (float (/ duration 1e9))
+                            (str/replace (:file (:kaocha.testable/meta test))
+                                         (str (.getCanonicalPath (io/file ".")) "/")
+                                         "")
+                            (:line (:kaocha.testable/meta test))))))])
+           (flatten)
+           (apply str)
+           print)
+      (flush))
     result))
