30b11821d12120fec61a3b242e6e4e2cb73cbc62
Arne Brasseur <arne@arnebrasseur.net>
2018-09-22 10:21:14 +0200

    Add missing hierarchy namespace

---
+++ src/kaocha/hierarchy.clj
@@ -0,0 +1,26 @@
+(ns kaocha.hierarchy
+  (:refer-clojure :exclude [isa?]))
+
+(defonce hierarchy (make-hierarchy))
+
+(defn derive!
+  "Add a parent/child relationship to kaocha's keyword hierarchy."
+  [tag parent]
+  (alter-var-root #'hierarchy derive tag parent))
+
+(derive! :fail :kaocha/fail-type)
+(derive! :error :kaocha/fail-type)
+
+(derive! :pass :kaocha/known-key)
+(derive! :fail :kaocha/known-key)
+(derive! :error :kaocha/known-key)
+(derive! :begin-test-suite :kaocha/known-key)
+(derive! :end-test-suite :kaocha/known-key)
+(derive! :begin-test-ns :kaocha/known-key)
+(derive! :end-test-ns :kaocha/known-key)
+(derive! :begin-test-var :kaocha/known-key)
+(derive! :end-test-var :kaocha/known-key)
+(derive! :summary :kaocha/known-key)
+
+(defn isa? [tag parent]
+  (clojure.core/isa? hierarchy tag parent))
