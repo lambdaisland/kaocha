07e0446d651e0825a6b5755e6314b974de815655
Arne Brasseur <arne@arnebrasseur.net>
2018-12-03 13:29:39 +0100

    Add mapply to core-ext (support for kaocha-cljs)
    Add test coverage for core-ext.

--- src/kaocha/core_ext.clj
+++ src/kaocha/core_ext.clj
@@ -21,4 +21,12 @@
   (cond
     (regex? x)  x
     (string? x) (Pattern/compile x)
-    :else       (throw (ex-data (str "Can't coerce " (class x) " to regex." {})))))
+    :else       (throw (ex-info (str "Can't coerce " (class x) " to regex.") {}))))
+
+(defn mapply
+  "Applies a function f to the argument list formed by concatenating
+  everything but the last element of args with the last element of
+  args.  This is useful for applying a function that accepts keyword
+  arguments to a map."
+  [f & args]
+  (apply f (apply concat (butlast args) (last args))))
---
+++ test/unit/kaocha/core_ext_test.clj
@@ -0,0 +1,36 @@
+(ns kaocha.core-ext-test
+  (:require [clojure.test :refer :all]
+            [kaocha.core-ext :refer :all]))
+
+(deftest regex?-test
+  (is (regex? #"foo"))
+  (is (not (regex? "foo"))))
+
+(deftest exception?-test
+  (is (exception? (Exception. "oh no")))
+  (is (not (exception? (Throwable. "oh no")))))
+
+(deftest error?-test
+  (is (error? (Error. "oh no")))
+  (is (not (error? (Exception. "oh no"))))
+  (is (not (error? (Throwable. "oh no")))))
+
+(deftest throwable?-test
+  (is (throwable? (Error. "oh no")))
+  (is (throwable? (Exception. "oh no")))
+  (is (throwable? (Throwable. "oh no"))))
+
+(deftest ns?-test
+  (is (ns? *ns*))
+  (is (not (ns? {}))))
+
+(deftest regex-test
+  (is (= "ok" (re-find (regex "[ko]+") "--ok--")))
+  (is (= "ok" (re-find (regex #"[ko]+") "--ok--")))
+  (is (thrown? clojure.lang.ExceptionInfo (regex 123) "")))
+
+(deftest mapply-test
+  (let [f (fn [& {:as opts}]
+            {:opts opts})]
+    (is (= {:opts {:foo :bar :abc :xyz}}
+           (mapply f {:foo :bar :abc :xyz})))))
