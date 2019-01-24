46f007fc613c8d9132fdb589690ac8eab0fb9352
Arne Brasseur <arne@arnebrasseur.net>
2019-01-24 18:51:06 +0800

    A boatload of improvements to --watch
    I've tried to address as many of the concerns and issues with --watch as
    possible. In the process I've cleaned up the code quite a bit, but it's also
    gotten quite a bit more complex, and it's still basically untested because this
    stuff is a pain to test, but I've tried to dogfood it as much as possible.
    
    The big issue is that is fixed is that load errors are handled much better. Now
    if a namespace fails to load you'll get to see the relevant exception and trace,
    and the whole thing is reported as if it's a failing test run. That way plugins
    and reporters like the notifier plugin can also report on it. We no longer try
    to do an actual run in this case, all tests are skipped.
    
    Pressing enter will re-run all tests (not just previously failing tests).
    
    You can set patterns for files to ignore in `tests.edn` (:kaocha.watch/ignore
    [...]). These follow the glob spec of java's PatchMatcher. The original idea was
    to source these from .gitignore, but .gitignore uses a pretty specific format,
    so we'd have to parse and convert that, that's something for later.
    
    Finally `tests.edn` will also be watched, and it gets reloaded on every run, so
    you can change your config and add plugins on the fly.

--- src/kaocha/watch.clj
+++ src/kaocha/watch.clj
@@ -3,6 +3,7 @@
   (:require [hawk.core :as hawk]
             [kaocha.api :as api]
             [kaocha.result :as result]
+            [kaocha.plugin :refer [defplugin]]
             [clojure.java.io :as io]
             [lambdaisland.tools.namespace.dir :as ctn-dir]
             [lambdaisland.tools.namespace.file :as ctn-file]
@@ -14,72 +15,184 @@
             [kaocha.testable :as testable]
             [kaocha.stacktrace :as stacktrace]
             [kaocha.output :as output]
-            [clojure.test :as t]))
+            [clojure.test :as t]
+            [kaocha.plugin :as plugin]
+            [kaocha.config :as config]
+            [kaocha.core-ext :refer :all])
+  (:import [java.nio.file FileSystems]))
 
-(defn- try-run [config]
-  (let [result (try
+(defn- try-run [config focus tracker]
+  (let [config (if (seq focus)
+                 (assoc config :kaocha.filter/focus focus)
+                 config)
+        config (-> config
+                   (assoc ::focus focus)
+                   (assoc ::tracker tracker)
+                   (update :kaocha/plugins #(cons ::plugin %)))
+        result (try
                  (api/run config)
                  (catch Throwable t
                    (println "[watch] Fatal error in test run" t)))]
     (println)
     result))
 
+(defn drain-queue! [q]
+  (doall (take-while identity (repeatedly #(.poll q)))))
+
+(defn track-reload! [tracker]
+  (binding [kaocha.api/*active?* true]
+    (ctn-reload/track-reload (assoc tracker ::ctn-file/load-error {}))))
+
+(defn print-scheduled-operations! [tracker focus]
+  (let [unload (set (::ctn-track/unload tracker))
+        load   (set (::ctn-track/load tracker))
+        reload (set/intersection unload load)
+        unload (set/difference unload reload)
+        load   (set/difference load reload)]
+    (when (seq unload)
+      (println "[watch] Unloading" unload))
+    (when (seq load)
+      (println "[watch] Loading" unload))
+    (when (seq reload)
+      (println "[watch] Reloading" reload))
+    (when (seq focus)
+      (println "[watch] Re-running failed tests" (set focus)))))
+
+(defn drain-and-rescan! [q tracker watch-paths]
+  (drain-queue! q)
+  (ctn-dir/scan-dirs tracker watch-paths))
+
+(defn glob?
+  "Does path match any of the glob patterns.
+
+  See [FileSystem/getPathMatcher](https://docs.oracle.com/javase/7/docs/api/java/nio/file/FileSystem.html#getPathMatcher(java.lang.String))
+  for a description of the patterns, these are similar but not the same as
+  typical shell glob patterns. "
+  [path patterns]
+  (let [fs (FileSystems/getDefault)
+        patterns (map #(.getPathMatcher fs (str "glob:" %)) patterns)]
+    (some #(.matches % path) patterns)))
+
+(defn wait-and-rescan! [q tracker watch-paths ignore]
+  (let [f (.take q)]
+    (if (and (file? f) (glob? (.toPath f) ignore))
+      (recur q tracker watch-paths ignore)
+      [(drain-and-rescan! q tracker watch-paths) f])))
+
+(defn reload-config [config]
+  (let [{:kaocha/keys [cli-options cli-args]} config
+        {:keys [config-file plugin]}          cli-options
+
+        config       (-> config-file
+                         (config/load-config)
+                         (config/apply-cli-opts cli-options)
+                         (config/apply-cli-args cli-args))
+        plugin-chain (plugin/load-all (concat (:kaocha/plugins config) plugin))]
+    [config plugin-chain]))
+
+(defn run-loop [config tracker q watch-paths]
+  (loop [tracker      tracker
+         config       config
+         plugin-chain plugin/*current-chain*
+         focus        nil]
+
+    (let [result  (try-run config focus tracker)
+          tracker (::tracker result)
+          error   (::error result)
+          ignore  (::ignore config)]
+
+      (cond
+        error
+        (do
+          (println "[watch] Error reloading, all tests skipped.")
+          (let [[tracker _] (wait-and-rescan! q tracker watch-paths ignore)
+                [config plugin-chain] (reload-config config)]
+            (recur tracker config plugin-chain nil)))
+
+        (and (seq focus) (not (result/failed? result)))
+        (do
+          (println "[watch] Failed tests pass, re-running all tests.")
+          (recur (drain-and-rescan! q tracker watch-paths) config plugin-chain nil))
+
+        :else
+        (let [[tracker trigger] (wait-and-rescan! q tracker watch-paths ignore)
+              [config plugin-chain] (reload-config config)
+              focus (when-not (= :enter trigger)
+                      (->> result
+                           testable/test-seq
+                           (filter result/failed-one?)
+                           (map ::testable/id)))]
+          (recur tracker config plugin-chain focus))))))
+
+(defplugin kaocha.watch/plugin
+  "This is an internal plugin, don't use it directly.
+
+Behind the scenes we add this plugin to the start of the plugin chain. It takes
+care of reloading namespaces inside a Kaocha run, so we can report any load
+errors as test errors."
+  (pre-load [{::keys [tracker focus] :as config}]
+    (print-scheduled-operations! tracker focus)
+    (let [tracker    (track-reload! tracker)
+          config     (assoc config ::tracker tracker)
+          error      (::ctn-reload/error tracker)
+          error-ns   (::ctn-reload/error-ns tracker)
+          load-error (::ctn-file/load-error tracker)]
+      (if (and error error-ns)
+        (-> config
+            (assoc ::error error ::error-ns error-ns)
+            (update :kaocha/tests
+                    (partial map (fn [t] (assoc t :kaocha.testable/skip true)))))
+        config)))
+
+  (pre-test [test test-plan]
+    (if-let [error (::error test-plan)]
+      (do
+        (t/do-report {:type :error
+                      :kaocha/testable {:kaocha.testable/id :kaocha/watch}
+                      :message (str "Failed reloading ns: " (::error-ns test-plan))
+                      :actual error})
+        (assoc test
+               :kaocha.result/count 1
+               :kaocha.result/error 1
+               ::testable/skip-remaining? true))
+      test)))
+
+(defn watch-paths [config]
+  (into #{}
+        (comp (remove :kaocha.testable/skip)
+              (map (juxt :kaocha/test-paths :kaocha/source-paths))
+              cat
+              cat
+              (map io/file))
+        (:kaocha/tests config)))
+
+(defn watch! [q watch-paths]
+  (hawk/watch! [{:paths   watch-paths
+                 :handler (fn [ctx event]
+                            (when (= (:kind event) :modify)
+                              (.put q (:file event))))}]))
+
 (defn run [config]
-  (let [watch-paths       (into #{} (comp (remove :kaocha.testable/skip)
-                                          (map (juxt :kaocha/test-paths :kaocha/source-paths))
-                                          cat
-                                          cat
-                                          (map io/file))
-                                (:kaocha/tests config))
-        watch-chan        (java.util.concurrent.ArrayBlockingQueue. 1024)
-        drain-watch-chan! (fn [] (doall (take-while identity (repeatedly #(.poll watch-chan)))))
-        tracker           (-> (ctn-track/tracker)
-                              (ctn-dir/scan-dirs watch-paths)
-                              (dissoc :lambdaisland.tools.namespace.track/unload
-                                      :lambdaisland.tools.namespace.track/load))]
+  (let [watch-paths (watch-paths config)
+        q           (java.util.concurrent.ArrayBlockingQueue. 1024)
+        tracker     (-> (ctn-track/tracker)
+                        (ctn-dir/scan-dirs watch-paths)
+                        (dissoc :lambdaisland.tools.namespace.track/unload
+                                :lambdaisland.tools.namespace.track/load))]
     (future
       (try
-        (loop [tracker tracker
-               focus   nil]
-          (let [unload     (set (::ctn-track/unload tracker))
-                load       (set (::ctn-track/load tracker))
-                load-error (::ctn-file/load-error tracker)
-                reload     (set/intersection unload load)
-                unload     (set/difference unload reload)
-                load       (set/difference load reload)
-                tracker    (ctn-reload/track-reload (assoc tracker ::ctn-file/load-error {}))]
-            (when (seq load-error)
-              (doseq [[f e] load-error]
-                (output/warn "Failed loading" f)
-                (stacktrace/print-cause-trace e t/*stack-trace-depth*)))
-
-            (when (seq unload) (println "[watch] Unloading" unload))
-            (when (seq load) (println "[watch] Loading" unload))
-            (when (seq reload) (println "[watch] Reloading" reload))
-            (when (seq focus) (println "[watch] Re-running failed tests" (set focus)))
-
-            (let [config' (cond-> config (seq focus) (assoc :kaocha.filter/focus focus))
-                  result  (try-run config')]
-              (if (and (seq focus) (not (result/failed? result)))
-                (do
-                  (println "[watch] Failed tests pass, re-running all tests.")
-                  (drain-watch-chan!)
-                  (recur (ctn-dir/scan-dirs tracker watch-paths) nil))
-                (let [f (.take watch-chan)]
-                  (drain-watch-chan!)
-                  (recur (ctn-dir/scan-dirs tracker watch-paths)
-                         (->> result
-                              testable/test-seq
-                              (filter result/failed-one?)
-                              (map ::testable/id))))))))
+        (run-loop config tracker q watch-paths)
         (catch Throwable t
           (.printStackTrace t))
         (finally
           (println "[watch] loop broken"))))
 
-    (hawk/watch! [{:paths   watch-paths
-                   :handler (fn [ctx event]
-                              (when (= (:kind event) :modify)
-                                (.put watch-chan (:file event))))}])
+    (watch! q watch-paths)
+    (watch! q [(get-in config [:kaocha/cli-options :config-file])])
+
+    (future
+      (while true
+        (.readLine (io/reader System/in))
+        (.put q :enter)))
 
     @(promise)))
