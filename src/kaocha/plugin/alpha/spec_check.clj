4f673f143787af8fdfc70fc0d680d56662a03948
Whitt, Alex <alex.joseph.whitt@gmail.com>
2019-05-30 10:08:36 -0400

    plugin to either add a default test suite or override existing ones

--- src/kaocha/plugin/alpha/spec_check.clj
+++ src/kaocha/plugin/alpha/spec_check.clj
@@ -2,7 +2,7 @@
   (:require [clojure.spec.alpha :as s]
             [kaocha.hierarchy :as kaocha]
             [kaocha.plugin :refer [defplugin]]
-            [kaocha.testable :as testable]
+            [kaocha.testable :as default-test-suite]
             [kaocha.type.clojure.test]
             [kaocha.type :as type]
             [kaocha.type.clojure.spec.alpha.check]
@@ -11,7 +11,20 @@
 (alias 'stc 'clojure.spec.test.check)
 (alias 'type.stc 'kaocha.type.clojure.spec.alpha.check)
 
-(defn testable [{ns-patterns ::ns-patterns :as config}]
+(def is-spec-check? (comp #{:kaocha.type/clojure.spec.test.alpha.check}
+                       :kaocha.testable/type))
+
+(defn has-spec-check? [{:kaocha/keys [tests] :as config}]
+  (some is-spec-check? tests))
+
+(defn overridden-spec-check-settings [{:kaocha/keys [tests] :as config}]
+  (map (fn [test]
+         (if (is-spec-check? test)
+           (update test merge (type.fdef/stc-opts config))
+           test))
+       tests))
+
+(defn default-test-suite [{ns-patterns ::ns-patterns :as config}]
   (-> {:kaocha.testable/type    :kaocha.type/clojure.spec.test.alpha.check
        :kaocha.testable/id      :generative
        :kaocha/ns-patterns      ns-patterns
@@ -20,22 +33,31 @@
        ::type.stc/syms          :all-fdefs}
       (merge (type.fdef/stc-opts config))))
 
+(defn overide-spec-check-settings [config]
+  (assoc config :kaocha/tests (overridden-spec-check-settings config)))
+
+(defn add-default-test-suite [config]
+  (update config :kaocha/tests conj (default-test-suite config)))
+
 (defplugin kaocha.plugin.alpha/spec-check
-  (pre-load [config] (update config :kaocha/tests conj (testable config)))
+  (pre-load [config]
+            (if (has-spec-check? config)
+              (overide-spec-check-settings config)
+              (add-default-test-suite config)))
   (cli-options [opts]
-    (conj opts
-          [nil  "--num-tests NUM" "Test iterations per fdef"
-           :parse-fn #(Integer/parseInt %)]
-          [nil  "--max-size SIZE" "Maximum length of generated collections"
-           :parse-fn #(Integer/parseInt %)]
-          [nil  "--ns-pattern PATTERN" "Regex matching your project's namespaces"]))
+               (conj opts
+                     [nil  "--num-tests NUM" "Test iterations per fdef"
+                      :parse-fn #(Integer/parseInt %)]
+                     [nil  "--max-size SIZE" "Maximum length of generated collections"
+                      :parse-fn #(Integer/parseInt %)]
+                     [nil  "--ns-pattern PATTERN" "Regex matching your project's namespaces"]))
   (config [config]
-    (let [num-tests  (get-in config [:kaocha/cli-options :num-tests])
-          max-size   (get-in config [:kaocha/cli-options :max-size])
-          ns-pattern (get-in config [:kaocha/cli-options :ns-pattern])]
-      (assoc config
-             ::ns-patterns [ns-pattern]
-             ::stc/num-tests num-tests
-             ::stc/max-size max-size))))
+          (let [num-tests  (get-in config [:kaocha/cli-options :num-tests])
+                max-size   (get-in config [:kaocha/cli-options :max-size])
+                ns-pattern (get-in config [:kaocha/cli-options :ns-pattern])]
+            (assoc config
+                   ::ns-patterns [ns-pattern]
+                   ::stc/num-tests num-tests
+                   ::stc/max-size max-size))))
 
 (s/def ::ns-patterns :kaocha/ns-patterns)
