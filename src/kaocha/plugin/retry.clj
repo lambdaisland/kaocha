db24f52728e234d23294f888f6eb98c3b582da88
Andrea Crotti <andrea.crotti.0@gmail.com>
2020-11-27 09:46:25 +0000

    hacky way to retry tests a given number of times

--- src/kaocha/plugin/retry.clj
+++ src/kaocha/plugin/retry.clj
@@ -2,24 +2,41 @@
   "Instrument/unstrument namespaces with Orchestra, to get validation of function
   arguments and return values based on clojure.spec.alpha."
   (:require [kaocha.plugin :refer [defplugin]]
-            [orchestra.spec.test :as orchestra]
-            [clojure.spec.alpha :as spec]))
+            [kaocha.hierarchy :as h]
+            [kaocha.testable :as t]
+            [kaocha.result :as r]))
+
+(def max-retries 3)
+(def wait-time 100)
+;; this needs to be reset in a fixture before every test
+(def current-retries (atom {}))
+
+(defn any-failed? []
+  (let [v (vals @current-retries)]
+    (and (seq v)
+         (= 3 (apply max v)))))
 
 (defplugin kaocha.plugin/retry
-  (post-run [result]
-    (println "test was run with result" (keys result))
-    (println "error = "
-             (-> result
-                 ;; :kaocha.result
-                 :kaocha.result/tests
-                 first
-                 :kaocha.result/tests
-                 ;; keys
-                 ;; first
-                 #_:kaocha.result/error)
-             "fail ="
-             #_(-> result
-                 :kaocha.result/tests
-                 ;; first
-                 #_:kaocha.result/fail))
-    result))
+  (pre-report [event]
+    (if (any-failed?)
+      event
+      (assoc event :type :pass)))
+
+  (pre-run [test-plan]
+    (reset! current-retries {})
+    test-plan)
+
+  (post-test [test test-plan]
+    (let [t-id (:kaocha.testable/id test)
+          curr (get @current-retries t-id 0)]
+      (when (and
+             (h/leaf? test)
+             (r/failed-one? test)
+             (< curr max-retries))
+
+        (println "Retrying time " @current-retries)
+        (swap! current-retries assoc t-id (inc curr))
+        (Thread/sleep wait-time)
+        (t/run-testable test test-plan))
+
+      test)))
