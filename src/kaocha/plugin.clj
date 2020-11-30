7242b192b6c19252da6d6a1d148ff0b53d795964
Alys Brooks <actuallyalys+github@gmail.com>
2020-11-30 17:29:52 -0600

    Add better plugin loading errors.

--- src/kaocha/plugin.clj
+++ src/kaocha/plugin.clj
@@ -16,22 +16,48 @@
     (catch java.io.FileNotFoundException e
       false)))
 
+(defn- try-require-info [n]
+  [(try-require n) n])
+
 (defn try-load-third-party-lib [plugin-name]
   (if (qualified-keyword? plugin-name)
-    (when-not (try-require (symbol (str (namespace plugin-name) "." (name plugin-name))))
+    (if-let [full-result (try-require (symbol (str (namespace plugin-name) "." (name plugin-name))))]
+      [[full-result (symbol (str (namespace plugin-name) "." (name plugin-name)))]]
+
+      [[false (symbol (str (namespace plugin-name) "." (name plugin-name)))]
+        (try-require-info (symbol (namespace plugin-name))) ] 
+      
+      )
+    #_(when-not (try-require (symbol (str (namespace plugin-name) "." (name plugin-name))))
       (try-require (symbol (namespace plugin-name))))
-    (try-require (symbol (name plugin-name)))))
+    [[plugin-name (try-require (symbol (name plugin-name))) ]]))
 
 (defmulti -register "Add your plugin to the stack"
   (fn [name plugins] name))
 
 (defmethod -register :default [name plugins]
-  (output/error "Couldn't load plugin " name)
   (throw+ {:kaocha/early-exit 254} nil (str "Couldn't load plugin " name)))
 
 (defn register [plugin-name plugin-stack]
-  (try-load-third-party-lib plugin-name)
-  (-register plugin-name plugin-stack))
+  (let [ns-result (try-load-third-party-lib plugin-name)
+        successful-ns (map second (filter first ns-result))
+        failed-ns (map second (filter (complement first) ns-result))
+        plugin-result (try+ (-register plugin-name plugin-stack)
+                            (catch [:kaocha/early-exit 254] _ false)) ] 
+    (cond
+      (and (not plugin-result)
+           (every? true? (map first ns-result))) (output/error-and-throw {:kaocha/early-exit 254} nil
+                                                  (str "Couldn't load plugin " plugin-name
+                                                     "but loaded " successful-ns)) 
+      (and (not plugin-result)
+           (> (count failed-ns) 1)) (output/error-and-throw 
+                                      {:kaocha/early-exit 254} nil
+                                      (format "Couldn't load plugin %s. Failed to load namespaces %s." 
+                                              plugin-name (apply str ( interpose " and " failed-ns))))
+      (not plugin-result) (output/error-and-throw 
+                            {:kaocha/early-exit 254} nil
+                            (format "Couldn't load plugin %s. Failed to load namespace %s." 
+                                    plugin-name (first failed-ns))))))
 
 (defn normalize-name [plugin-name]
   (if (and (simple-keyword? plugin-name)
