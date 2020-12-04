c41d6e3b933901b3da4adcd875ed509631cbf631
Alys Brooks <actuallyalys+github@gmail.com>
2020-12-04 12:18:32 -0600

    Add tests for various errors that appear when loading plugins.

--- test/unit/kaocha/plugin_test.clj
+++ test/unit/kaocha/plugin_test.clj
@@ -6,13 +6,41 @@
   (:import (clojure.lang ExceptionInfo)))
 
 (deftest missing-plugin-test
+  (let [expected-message "Couldn't load plugin :kaocha.missing.plugin/gone. Failed to load namespaces kaocha.missing.plugin.gone and kaocha.missing.plugin."]
   (is (thrown-with-msg? ExceptionInfo
-                        #"Couldn't load plugin :kaocha.missing.plugin/gone"
+                        (re-pattern expected-message)
                         (plugin/load-all [:kaocha.missing.plugin/gone])))
-  (is (= {:err "ERROR: Couldn't load plugin :kaocha.missing.plugin/gone. Failed to load namespaces kaocha.missing.plugin.gone and kaocha.missing.plugin.\n" :out "" :result nil}
+  (is (= {:err  (str "ERROR: " expected-message "\n") :out "" :result nil}
          (binding [output/*colored-output* false]
            (util/with-out-err
              (try
                (plugin/load-all [:kaocha.missing.plugin/gone])
                (catch ExceptionInfo e
-                 nil)))))))
+                 nil))))))))
+
+(deftest missing-unnamespaced-plugin-test
+  (let [expected-message "Couldn't load plugin :kaocha.plugin/gone. Failed to load namespace kaocha.plugin.gone."]
+    (is (thrown-with-msg? ExceptionInfo
+                          (re-pattern expected-message)
+                        (plugin/load-all [:gone])))
+  (is (= {:err (str "ERROR: " expected-message "\n") :out "" :result nil}
+         (binding [output/*colored-output* false]
+           (util/with-out-err
+             (try
+               (plugin/load-all [:gone])
+               (catch ExceptionInfo e
+                 nil))))))))
+
+
+(deftest missing-plugin-valid-ns-test
+  (let [expected-message "Couldn't load plugin :kaocha/plugin, but loaded kaocha.plugin."]
+    (is (thrown-with-msg? ExceptionInfo
+                          (re-pattern expected-message)
+                          (plugin/load-all [:kaocha/plugin])))
+    (is (= {:err (str "ERROR: " expected-message "\n") :out "" :result nil}
+           (binding [output/*colored-output* false]
+             (util/with-out-err
+               (try
+                 (plugin/load-all [:kaocha/plugin])
+                 (catch ExceptionInfo e
+                   nil))))))))
