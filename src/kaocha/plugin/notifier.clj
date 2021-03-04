3f9b276cf3f1f4064120d589b809f80f2038ab95
Alys Brooks <alys@gaiwan.co>
2021-03-04 15:18:44 -0600

    Add support for Java's notification messages.

--- src/kaocha/plugin/notifier.clj
+++ src/kaocha/plugin/notifier.clj
@@ -10,7 +10,8 @@
             [clojure.java.io :as io]
             [slingshot.slingshot :refer [throw+]])
   (:import [java.nio.file Files]
-           [java.io IOException] ))
+           [java.io IOException] 
+           [java.awt SystemTray TrayIcon  TrayIcon$MessageType Toolkit])) 
 
 ;; special thanks for terminal-notify stuff to
 ;; https://github.com/glittershark/midje-notifier/blob/master/src/midje/notifier.clj
@@ -22,7 +23,8 @@
     (try 
       (= 0 (:exit (sh cmd program)))
       (catch IOException e  ;in the unlikely event where.exe or which isn't available
-        (output/warn (format "Unable to determine whether '%s' exists." program)) ))))
+        (output/warn (format "Unable to determine whether '%s' exists. Notifications may not work." program)) ))))
+
 
 (defn detect-command []
   (cond
@@ -61,6 +63,29 @@
          (let [file (java.io.File/createTempFile "clojure_logo" ".png")]
            (io/copy (io/make-input-stream resource {}) file)
            (str file)))))))
+
+
+(def tray-icon
+  "Creates a system tray icon."
+  (memoize
+    (fn [icon-path]
+      (let [tray-icon (-> (Toolkit/getDefaultToolkit) 
+                          (.getImage icon-path)
+                          (TrayIcon. "Kaocha Notification"))]
+        (doto (SystemTray/getSystemTray)
+          (.add tray-icon))
+        tray-icon))))
+
+(defn send-tray-notification 
+  "Use Java's built-in functionality to display a notification.
+  
+  Not preferred over shelling out because the built-in notification sometimes
+  looks out of place, and isn't consistently available on Linux."
+  [result]
+  (let [icon (tray-icon "kaocha/clojure_logo.png")
+        urgency (if (result/failed? result) TrayIcon$MessageType/ERROR TrayIcon$MessageType/INFO) ]
+  (.displayMessage icon (title result) (message result) urgency)))
+
 
 (defn expand-command
   "Takes a command string including replacement patterns, and a map of
@@ -119,5 +144,6 @@
 
   (post-run [result]
     (if-let [command (and (::notifications? result) (::command result))]
-      (run-command command result))
+      (run-command command result)
+      (send-tray-notification result))
     result))
