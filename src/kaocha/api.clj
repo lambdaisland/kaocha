d144fbcdb40ea63253d61084d0e26578f2e2ae68
Alys Brooks <alys@gaiwan.co>
2022-06-16 17:59:55 -0500

    Reword errors following Arne's advice.
    Fix the "No tests found"/"tests were skipped" contradiction and clarify
    that we're speaking of Kaocha's configuration.

--- src/kaocha/api.clj
+++ src/kaocha/api.clj
@@ -104,13 +104,12 @@
                                      (::testable/load-error %))
                                 (testable/test-seq test-plan))
                   (if (not (zero? (count (filter ::testable/skip (testable/test-seq-with-skipped test-plan)))))
-                    (output/warn (str "No tests were found and some tests were"
-                                      " skipped; check for misspelled settings"
-                                      " or combinations of settings that no tests"
-                                      " satisfy."))
-                    (output/warn (str "No tests were found. This may be an issue in your test configuration."
-                                      " To investigate, check the :test-paths and :ns-patterns keys in tests.edn."))
-                    )
+                    (output/warn (format (str "All %d tests were skipped."
+                                              " Check for misspelled settings in your Kaocha test configuration"
+                                              " or incorrect focus or skip filters.")
+                                         (count (testable/test-seq-with-skipped test-plan))))
+                    (output/warn (str "No tests were found. This may be an issue in your Kaocha test configuration."
+                                      " To investigate, check the :test-paths and :ns-patterns keys in tests.edn.")))
                   (throw+ {:kaocha/early-exit 0 }))
                 
                 (when (find-ns 'matcher-combinators.core)
