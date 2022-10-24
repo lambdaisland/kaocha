42e0a964e270eaeb679a8cad900f1b954b833f71
Alys Brooks <alys@gaiwan.co>
2022-10-24 18:03:39 -0500

    Catch invalid spec exceptions when loading config.
    Instead of exiting early, warns the user and re-uses the old
    configuration. Small downside: Users may not notice validation errors in
    your test suite because your test suite will run immediately thereafter.
    This is mostly an issue with the more verbose reporters or large test
    suites. It could also become an issue if your test fails, although
    there's a decent chance users would go through the scrollback in that case
    and notice the warning.

--- src/kaocha/watch.clj
+++ src/kaocha/watch.clj
@@ -23,6 +23,7 @@
             [lambdaisland.tools.namespace.parse :as ctn-parse]
             [lambdaisland.tools.namespace.reload :as ctn-reload]
             [lambdaisland.tools.namespace.track :as ctn-track]
+            [slingshot.slingshot :refer [try+]]
             [nextjournal.beholder :as beholder])
   (:import (java.nio.file FileSystems)
            (java.util.concurrent ArrayBlockingQueue BlockingQueue)))
@@ -178,7 +179,10 @@
   (if-let [config-file (get-in config [:kaocha/cli-options :config-file])]
     (let [{:kaocha/keys [cli-options cli-args]} config
           profile (get-in config [:kaocha/cli-options :profile])
-          config (config/load-config2 config-file profile {} cli-options cli-args)
+          config (try+ 
+                   (config/load-config2 config-file profile {} cli-options cli-args)
+                   (catch :kaocha/early-exit  e
+                     (output/warn "Error loading config: " e "\nFalling back to prior config.")))
           plugin-chain (plugin/load-all (concat (:kaocha/plugins config) (:plugin cli-options)))]
       [config plugin-chain])
     [config plugin-chain]))
