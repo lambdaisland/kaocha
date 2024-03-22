316a49d2ec3367d77e06f64fa39a0fe4133c14c9
Noah Bogart <noah.bogart@hey.com>
2024-03-22 11:08:12 -0400

    Demonstrate a once fixture that calls f twice

---
+++ test/unit/kaocha/fixtures_test.clj
@@ -0,0 +1,45 @@
+(ns kaocha.fixtures-test
+  (:refer-clojure :exclude [symbol])
+  (:require [clojure.test :as t :refer [testing is deftest]]
+            [kaocha.test-factories :as f]
+            [kaocha.testable :as testable]
+            [kaocha.classpath :as classpath]
+            [kaocha.test-helper]
+            [kaocha.core-ext :refer :all]
+            [kaocha.test-util :refer [with-test-ctx]]
+            [kaocha.type.var]
+            [matcher-combinators.test :refer [match?]]))
+
+(deftest once-fixtures-test
+  (classpath/add-classpath "fixtures/d-tests")
+  (testing "once fixture calling f twice"
+    (require 'ddd.double-once-fixture-test)
+    (let [{:keys [result report]}
+          (with-test-ctx {:fail-fast? false}
+            (testable/run {:kaocha.testable/type :kaocha.type/var
+                           :kaocha.testable/id   :ddd.double-once-fixture-test/example-fail-test
+                           :kaocha.testable/desc "example-fail-test"
+                           :kaocha.var/name      'ddd.double-once-fixture-test/example-fail-test
+                           :kaocha.var/var       (resolve 'ddd.double-once-fixture-test/example-fail-test)
+                           :kaocha.var/test      (-> (resolve 'ddd.double-once-fixture-test/example-fail-test) meta :test)}
+                          (f/test-plan {})))]
+
+      (is (match? {:kaocha.testable/type :kaocha.type/var
+                   :kaocha.testable/id   :ddd.double-once-fixture-test/example-fail-test
+                   :kaocha.testable/desc "example-fail-test"
+                   :kaocha.var/name      'ddd.double-once-fixture-test/example-fail-test
+                   :kaocha.var/var       (resolve 'ddd.double-once-fixture-test/example-fail-test)
+                   :kaocha.var/test      fn?
+                   :kaocha.result/count  1
+                   :kaocha.result/pass   0
+                   :kaocha.result/error  0
+                   :kaocha.result/fail   1}
+                  result))
+
+      (is (match? [{:type :begin-test-var}
+                   {:type :pass
+                    :expected '(= 1 2)
+                    :actual '(not (= 1 2))
+                    :message nil}
+                   {:type :end-test-var}]
+                  report)))))
